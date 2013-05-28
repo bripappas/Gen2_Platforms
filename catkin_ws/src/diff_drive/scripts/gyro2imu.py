2643bcd47af8f726fa6470ba5cdf1ea750ec1229
Overkill <overkill@overkill.(none)>
2013-05-28 18:24:56 -0500

    Gyro calculating yaw

--- catkin_ws/src/diff_drive/scripts/gyro2imu.py
+++ catkin_ws/src/diff_drive/scripts/gyro2imu.py
@@ -1,22 +1,66 @@
 #!/usr/bin/env python
 import rospy
+import numpy
+import math
 from std_msgs.msg import Int16, Float32
+from sensor_msgs.msg import Imu
 
-rVel=0;
-lVel=0;
-gyroRaw=0;
+rVel=0					#Right Wheel Vel (m/s)
+lVel=0					#Left Wheel Vel (m/s)
+gyroRaw=0				#Analog Gyro  Reading
+
+buffer_length=500		#Maximum number to calculate calibrated average
+buffer=[]				#Array to average to calculate calibrated gyro value
+gyro_cal=0;				#mean of buffer
+gyroScale=1;			#Factor to scale gyro by
+
+imuPub=rospy.Publisher('imu',Imu)
+previousTime=rospy.Time()
+yaw=0;
 
 
+
+#Since the gyro sensor is sensitive to various envirnoment changes (e.g. temperature),
+#the neutral value of the sensor is constantly updated when both wheel velocites are 0;
 def gyroCallback(msg):
-    rospy.loginfo("I heard %s" % msg.data)
-    
+	#rospy.loginfo("Gyro Val %s" % msg.data)
+	global buffer,gyroRaw,gyro_cal,previousTime,yaw
+	currentTime=rospy.Time.now()
+	dt=currentTime-previousTime
+	dt=dt.to_sec()
+	previousTime=currentTime
+	gyroRaw=msg.data
+	if rVel==0 and lVel==0:
+		buffer.append(gyroRaw)
+		
+		if len(buffer) > buffer_length:
+			buffer.pop(0)
+			
+			
+	gyro_cal=numpy.mean(buffer)
+	angular_vel=-(((gyroRaw-gyro_cal)/gyro_cal)*300*math.pi/180*gyroScale);
+	
+	if math.fabs(angular_vel) < 0.0:
+		angular_vel=0
+	yaw+=(angular_vel*dt)
+	
+	print(dt)
+	
+	imu=Imu()
+	imu.header.stamp = currentTime
+	imu.header.frame_id = 'imu'
+			
+#Grab the Left Wheel Veloctiy
 def lVelCallback(msg):
+	global lVel
 	lVel=msg.data
-	rospy.loginfo("I heard %s" % lVel)
-    
+	#rospy.loginfo("Left Vel %s" % lVel)
+
+#Grab the Right Whell Velocity    
 def rVelCallback(msg):
+	global rVel
 	rVel=msg.data
-	rospy.loginfo("I heard %s" % rVel)
+	#rospy.loginfo("Right Vel %s" % rVel)
 
 
 def gyro():
@@ -24,6 +68,7 @@ def gyro():
     rospy.Subscriber("gyro_val", Int16, gyroCallback)
     rospy.Subscriber("lwheel_vel", Float32, lVelCallback);
     rospy.Subscriber("rwheel_vel", Float32, rVelCallback);
+    previousTime=rospy.Time.now()
     rospy.spin()
 
 
