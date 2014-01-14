65527a4c9e585e2fa6f8cde6bdd6fb2a35eb4e84
BRAIN <brain@ubuntu.(none)>
2014-01-14 15:43:20 -0600

    Added freespace to rvis config

--- catkin_hydro/src/gen2_frontier/scripts/freespace.py
+++ catkin_hydro/src/gen2_frontier/scripts/freespace.py
@@ -2,6 +2,7 @@
 
 import roslib; roslib.load_manifest('gen2_frontier')
 import rospy
+import random
 import std_msgs.msg
 from nav_msgs.msg import OccupancyGrid
 from nav_msgs.srv import GetMap
@@ -15,16 +16,19 @@ def freeSpace():
 	freeSpacePub = rospy.Publisher('freeSpace',OccupancyGrid,latch=True)
 	
 	# Create service client to get map from map server
-	print "waiting for map"
+	rospy.loginfo("Waiting for map to become available")
 	rospy.wait_for_service('/static_map')
 	try:
 		getStaticMap = rospy.ServiceProxy('/static_map',GetMap)
 
 		resp = getStaticMap()
 		data=resp.map
+		
 	except rospy.ServiceException, e:
 		print "Static map service call failed: %s"%e
-		
+	
+	rospy.loginfo("The static map has been retrieved")
+	
 	#Use map data to generate message for freespacd map
 	mapMsg=OccupancyGrid()
 	mapMsg.header.stamp=rospy.Time.now()
@@ -33,12 +37,23 @@ def freeSpace():
 	mapMsg.info.width=data.info.width
 	mapMsg.info.height=data.info.height
 	mapMsg.info.origin=data.info.origin
-	mapMsg.data=data.data
 	
+	#Pack data inot message sturcture
+	mapList=list(data.data)
+	for i in range(len(mapList)):
+		if mapList[i]==0:
+			mapList[i]=random.randint(1,99)
+		
+	mapMsg.data=mapList
 	
-	print "publishing"
+	rospy.loginfo("Publishing Initial Freespace Map")
 	freeSpacePub.publish(mapMsg)
+	
+	rospy.loginfo("Waiting on Freespace Updates")
 	rospy.spin()
+	
+	
+	
 	
 	
 		
