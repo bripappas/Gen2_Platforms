c636a61286df4946025c3826bd733513cfc20139
BRAIN <brain@ubuntu.(none)>
2014-01-15 22:41:48 -0600

    Correct Transformation of laser scan to mpa frame

--- catkin_hydro/src/gen2_frontier/scripts/robotSenseBoundary.py
+++ catkin_hydro/src/gen2_frontier/scripts/robotSenseBoundary.py
@@ -6,6 +6,7 @@ import std_msgs.msg
 from sensor_msgs.msg import LaserScan
 import math
 from geometry_msgs.msg import PoseWithCovarianceStamped
+from tf.transformations import euler_from_quaternion
 
 robX = []  #Robot X Position
 robY = []  #Robot Y Position
@@ -39,13 +40,15 @@ def handlePoseMessage(data):
 	robY=data.pose.pose.position.y
 	
 def handleLaserScanMessage(data):
-	global listener
+	global listener,trans,rot
 	laserScan = []
 	angle_min = []
 	angle_max = []
 	angle_inc = []
 	xList = []
 	yList = []
+	xListXfrm =[]
+	yListXfrm =[]
 	laserScan = data.ranges
 	angle_min = data.angle_min
 	angle_max = data.angle_max
@@ -54,17 +57,35 @@ def handleLaserScanMessage(data):
 	
 	#Find (x,y) coordinates of Laser Scanner centered in the Lidar Frame
 	for i in range(len(laserList)):
-		xList.append(math.sin((angle_min+(angle_inc*i))))
-		yList.append(math.cos((angle_min+(angle_inc*i))))
+		xList.append(-math.sin((angle_min+(angle_inc*i)))*laserList[i])
+		yList.append(math.cos((angle_min+(angle_inc*i)))*laserList[i])
 		
 	#Transform (x,y) coordinates from Lidar Fram to global frame
 	try:
-		(trans,rot) = listener.lookupTransform('/robot_0/base_laser_link', '/map', rospy.Time())
+		(trans,quat) = listener.lookupTransform('/map','/robot_0/base_laser_link', rospy.Time())
 	except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 		print 'TF Lookup Failed'
 		
-	print rot
-	print trans
+	#Convert Cooridinates to map frame cartesian coordiantes(meters)
+	(roll,pitch,yaw) = euler_from_quaternion(quat)
+	for i in range(len(laserList)):
+		yListXfrm.append(trans[1]+(-xList[i]*math.cos(-yaw)+yList[i]*math.sin(-yaw)))
+		xListXfrm.append(trans[0]+(-xList[i]*math.sin(-yaw)-yList[i]*math.cos(-yaw)))
+		
+	print xListXfrm[0]
+	print yListXfrm[0]
+	
+	#Pull out needed map paramters
+	#width=mapData.info.width
+	#height=mapData.info.height
+	#resolution=mapData.info.resolution
+	
+	#Pull out current robot position
+	#robX=data.pose.pose.position.x*(1.0/resolution)+width/2
+	#robY=data.pose.pose.position.y*(1.0/resolution)+height/2
+	
+		
+
 		
 		
 	
