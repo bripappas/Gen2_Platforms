5e9f0784b4047354e58c19ea604d3a84d733eef9
BRAIN <brain@ubuntu.(none)>
2014-02-04 22:19:47 -0600

    Added Cost Matrix Calculation

--- catkin_hydro/src/gen2_frontier/scripts/frontierPlanner.py
+++ catkin_hydro/src/gen2_frontier/scripts/frontierPlanner.py
@@ -6,6 +6,8 @@ import rospy
 from visualization_msgs.msg import Marker	#Message Imports
 from geometry_msgs.msg import PoseStamped
 from nav_msgs.srv import GetPlan
+import math									#Other Imports
+import numpy
 
 class nodeClass():
 	#Class Init Function
@@ -17,33 +19,48 @@ class nodeClass():
 		rospy.spin()
 		
 	def markerCallback(self,data):
-		frontierPoints = data.points
 		
-		start = PoseStamped()
-		stop = PoseStamped()
+		costMatrix = self.createCostMatrix(data.points)
+		print costMatrix
 		
-		start.header.frame_id = '/robot_0/map'
-		start.pose.position = frontierPoints[0]
-	
-		stop.header.frame_id = '/robot_0/map'
-		stop.pose.position = frontierPoints[1]
+	def createCostMatrix(self,frontierPoints):
+		#Make list of robots get_plan services (PUT ON PARAMATER SERVER)
+		makePlanList = []
+		makePlanList.append('/robot_0/move_base_node/make_plan')
+		makePlanList.append('/robot_1/move_base_node/make_plan')
+		makePlanList.append('/robot_2/move_base_node/make_plan')
+		PlanFrameId = []
+		PlanFrameId.append('/robot_0/map')
+		PlanFrameId.append('/robot_1/map')
+		PlanFrameId.append('/robot_2/map')
 		
+		C = numpy.zeros((len(makePlanList), len(frontierPoints)))
 		
-		self.determineCost('/robot_0/move_base_node/make_plan',start,stop)
+		for i in range(0,len(frontierPoints)):
+			for j in range(0,len(makePlanList)):
+				stop = PoseStamped()
+				stop.header.frame_id = PlanFrameId[j]
+				stop.pose.position = frontierPoints[i]
+				serviceName = makePlanList[j]
+				C[j,i] = self.determineCost(serviceName,PoseStamped(),stop)
+		return C
+				
 		
 	def determineCost(self,serviceName, start, stop):
 		rospy.wait_for_service(serviceName)
-
 		try:
+			dist = 0
 			makePath = rospy.ServiceProxy(serviceName, GetPlan)
 			path = makePath(start, stop, 0.5)
-			print len(path.plan.poses)
-			#CALCULATE DISTANCE
+			poseArray = path.plan.poses
+			for i in range(0,len(poseArray)-1):
+				xDiff = poseArray[i].pose.position.x-poseArray[i+1].pose.position.x
+				yDiff = poseArray[i].pose.position.y-poseArray[i+1].pose.position.y
+				dist += math.sqrt(xDiff**2+yDiff**2)
+			return dist
 		except rospy.ServiceException, e:
 			print "Get Plan service call failed: %s" %e
-		
-
-
+			
 # Main Function
 if __name__ == '__main__':
 	# Initialize the Node and Call nodeClass()
